7b41e9118259936db41fd7335f534f3715512fa0
Lukas Hermann <lukas.hermann@gmx.de>
2019-02-12 14:34:21 +0100

    fix in dummy vec env

--- baselines/common/vec_env/dummy_vec_env.py
+++ baselines/common/vec_env/dummy_vec_env.py
@@ -62,7 +62,10 @@ class DummyVecEnv(VecEnv):
 
             obs, self.buf_rews[e], self.buf_dones[e], self.buf_infos[e] = self.envs[e].step(action)
             if self.buf_dones[e]:
-                obs = self.envs[e].reset(data=self.data)
+                if self.data:
+                    obs = self.envs[e].reset(data=self.data)
+                else:
+                    obs = self.envs[e].reset()
             self._save_obs(e, obs)
         return (self._obs_from_buf(), np.copy(self.buf_rews), np.copy(self.buf_dones),
                 self.buf_infos.copy())
